aeabd8f8989ed4cc4bd6839859cad7d3c7aa2ce1
r00kieee1 <omustapha2005@gmail.com>
2023-10-18 18:13:16 +0100

    Added a grade button, doesnot function quite yet

--- Report-card/card.js
+++ Report-card/card.js
@@ -1,10 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
     const calculateTotalButton = document.getElementById("calculate-total");
     const calculatePercentButton = document.getElementById("calculate-percent");
+    const findGradeButton = document.getElementById("grade");
     const testMarks = document.querySelectorAll(".test-mark");
     const examMarks = document.querySelectorAll(".exam-mark");
     const totalMarksDisplay = document.getElementById("total-marks");
     const percentageDisplay = document.getElementById("percentage");
+    const gradeDisplay = document.getElementById("grades")
 
     calculateTotalButton.addEventListener("click", function () {
         let totalMarks = 0;
@@ -20,5 +22,21 @@ document.addEventListener("DOMContentLoaded", function () {
         const totalMarks = parseFloat(totalMarksDisplay.textContent) || 0;
         const percentage = (totalMarks / (1200)) * 100;
         percentageDisplay.textContent = percentage.toFixed(3);
+    });
+
+    findGradeButton.addEventListener('click', function () {
+        if(parseFloat(percentageDisplay) <= 39) {
+            gradeDisplay.textContent = 'Fail';
+        } else if(parseFloat(percentage) <= 49) {
+            gradeDisplay.textContent = 'D';
+        } else if(parseFloat(percentage) <= 59) {
+            gradeDisplay.textContent = 'C';
+        } else if(parseFloat(percentage) <= 69) {
+            gradeDisplay.textContent = 'B';
+        } else if(parseFloat(percentage) <= 89) {
+            gradeDisplay.textContent = 'A';
+        } else {
+            gradeDisplay.textContent = 'A+';
+        }
     });
 });
