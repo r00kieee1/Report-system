1df6289e71239ac6222b28b0f7365afe09300e6e
r00kieee1 <omustapha2005@gmail.com>
2023-11-03 18:48:09 +0100

    Major changes on page functionality

--- index.js
+++ index.js
@@ -1,72 +1,106 @@
 document.addEventListener("DOMContentLoaded", function () {
-const addInput = document.querySelector('#addInput');
-const addButton = document.querySelector('#addBtn');
-const refreshBtn = document.querySelector('#refreshBtn');
-const divList = document.querySelector('.listHolder');
-const listUl = document.querySelector('.list');
-const lis = listUl.children;
+  const addInput = document.querySelector('#addInput');
+  const addButton = document.querySelector('#addBtn');
+  const refreshBtn = document.querySelector('#refreshBtn');
+  const divList = document.querySelector('.listHolder');
+  const listUl = document.querySelector('.list');
+  const names = JSON.parse(localStorage.getItem('names')) || [];
 
-refreshBtn.addEventListener('click', () => {
-  window.location.reload();
-});
+  function addName() {
+    const nameText = addInput.value.trim();
+    if (nameText === '') return;
 
-function createBtn(li) {
-  const remove = document.createElement('button');
-  remove.className = 'btn-icon remove';
-  li.appendChild(remove);
- 
-  return li;
-}
-for(var i = 0; i < lis.length; i++) {
-  createBtn(lis[i]);
-}
+    const name = { text: nameText };
+    names.push(name);
 
-divList.addEventListener('click', (event) => {
-  if(event.target.tagName === 'BUTTON') {
-    const button = event.target;
-    const li = button.parentNode;
-    const ul = li.parentNode;
-    if(button.className === 'btn-icon remove') {
-      ul.removeChild(li);
-    }
+    localStorage.setItem('names', JSON.stringify(names));
+
+    addInput.value = '';
+
+    displayNames();
   }
-});
 
-  const ul = document.querySelector('.list');
-  const reportCard = document.getElementById("reportCard");
+  function deleteName(index) {
+    names.splice(index, 1);
 
-  addButton.addEventListener("click", function () {
-    if(addInput.value === '') {
-      alert ('Enter a student name please!');
-    } else {
-      const li = document.createElement("li");
-      const ul = divList.querySelector('ul');
-      const listNameSpan = document.createElement('span');
-      listNameSpan.className = 'listName'; 
-      listNameSpan.textContent = addInput.value; 
-      addInput.value = '';
-      li.appendChild(listNameSpan); 
-      ul.appendChild(li);
-      createBtn(li);
+    localStorage.setItem('names', JSON.stringify(names));
 
-      const listItems = document.querySelectorAll(".list li");
-      listItems.forEach(function (item, index) {
-        item.addEventListener("click", function () {
-            const itemText = item.querySelector(".listName").textContent;
-            reportCard.search = `?item${index}=${encodeURIComponent(itemText)}`;
-            reportCard.click();
-        });
-      });
-    }    
+    displayNames();
+  }
+
+  function displayNames() {
+    listUl.innerHTML = '';
+
+    names.forEach((name, index) => {
+      const li = document.createElement('li');
+      li.innerHTML = `
+        <span>${name.text}</span>
+        <br>
+        <button class="delete-button">Delete Name</button>
+      `;
+
+      listUl.appendChild(li);
+    });
+  }
+
+  refreshBtn.addEventListener('click', function () {
+    window.location.reload();
   });
 
-  const listItems = document.querySelectorAll(".list li");
-  listItems.forEach(function (item, index) {
-    item.addEventListener("click", function () {
-        const itemText = item.querySelector(".listName").textContent;
-        reportCard.search = `?item${index}=${encodeURIComponent(itemText)}`;
+  divList.addEventListener('click', function (event) {
+    if (event.target.className === 'delete-button') {
+      const li = event.target.parentNode;
+      const index = Array.from(listUl.children).indexOf(li);
+      deleteName(index);
+    }
+  });
+
+  addButton.addEventListener('click', function () {
+    if (addInput.value === '') {
+      alert('Enter a student name, please!');
+    } else {
+      const li = document.createElement("li");
+      const listNameSpan = document.createElement('span');
+      listNameSpan.className = 'listName';
+      listNameSpan.textContent = addInput.value;
+      li.appendChild(listNameSpan);
+      listUl.appendChild(li);
+      createDeleteButton(li);
+
+      li.addEventListener("click", function () {
+        const itemText = listNameSpan.textContent;
+        reportCard.search = `?item${names.indexOf(nameText)}=${encodeURIComponent(itemText)}`;
         reportCard.click();
-    });
+      });
+
+      const nameText = addInput.value.trim();
+      const name = { text: nameText };
+      names.push(name);
+
+      localStorage.setItem('names', JSON.stringify(names));
+
+      addInput.value = '';
+      displayNames();
+
+      const index = names.indexOf(name);
+      navigateToNextPage(index, nameText);
+    }
+  });
+
+  function navigateToNextPage(index, nameText) {
+    const nextPageURL = 'Report-card/card.html'; 
+    const encodedName = encodeURIComponent(nameText);
+    const queryParam = `?item${index}=${encodedName}`;
+    window.location.href = nextPageURL + queryParam;
+  }
+
+  function createDeleteButton(li) {
+    const deleteButton = document.createElement('button');
+    deleteButton.className = 'delete-button';
+    deleteButton.textContent = 'Delete';
+    li.appendChild(deleteButton);
+  }
+
+  displayNames();
 });
 
-});
